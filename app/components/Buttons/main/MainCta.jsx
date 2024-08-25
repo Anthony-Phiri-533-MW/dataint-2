4b08f420d3fdfba61fb954b815cb1e1dfe51569d
Anthony-Phiri-533-MW <anthonyphiri533@gmail.com>
2024-08-25 14:23:41 +0200

    Add Hero added hero component. also added links to the nav elements

---
+++ app/components/Buttons/main/MainCta.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const MainCta = ({text}) => {
+  return (
+    <button className='font-bold bg-[#001538] text-[#FFFFFF] rounded-full p-2 uppercase tracking-tight hover:tracking-wide'>{text}</button>
+  )
+}
+
---
+++ app/components/Hero/Hero.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { MainCta } from "../Buttons/main/MainCta";
+import Image from 'next/image';
+import heroImage from "../../../public/bgremoved1 1.png";
+
+const Hero = () => {
+    return (
+        <div className='lg:flex md:flex lg:justify-between lg:space-x-12 md:space-x-8'>
+            <section className='space-y-12'>
+                <h1 className='lg:text-5xl text-4xl grid grid-cols-1 font-bold lg:tracking-wide'>Unlocking tomorrow's <span>solutions today</span></h1>
+                <h3 className='lg:text-2xl text-xl grid grid-cols-1'>Experience Data-Driven Excellence 
+                    <span>Empowering Insights, </span>
+                    <span>Transforming Businesses </span>
+                    <span>and various fields</span>
+                </h3>
+                <MainCta text={"work with us"}/>
+            </section>
+                <Image src={heroImage} alt='hero image' width={400} height={300} />
+            <section>
+
+            </section>
+        </div>
+    )
+}
+
+export default Hero
